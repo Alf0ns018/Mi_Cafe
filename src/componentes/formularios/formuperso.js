edbe4478904d8651dc6b7f8dcfdeadb27af1a51b
Alfonso <edgaralfonsoeacg@gmail.com>
2021-05-07 16:50:19 -0500

    Cambio de las pag de inicio y registrar

--- src/componentes/formularios/formuperso.js
+++ src/componentes/formularios/formuperso.js
@@ -6,34 +6,34 @@ class formuperso extends Component {
     render() {
         return (
             <div>
-                <div class="card mt-3">
+                <div class="card text-center bg-primary mb-3">
                     <div class="card-header">
                         Registro
-                             </div>
-                    <div class="card-body">
+                     </div>
+
+                    <div class="card-body bg-light mb-3">
                         <form>
                             <div className="form-group">
                                 <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                <input type="email" className="form-control text-center" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                             </div>
                             <div className="form-group">
                                 <label for="exampleInputPassword1">Nombre</label>
-                                <input type="text" className="form-control" name=" Nombre " placeholder="Introduzca su Nombre" />
+                                <input type="text" className="form-control text-center" name=" Nombre " placeholder="Introduzca su Nombre" />
                             </div>
                             <div className="form-group">
                                 <label for="exampleInputPassword1">Apellido Paterno</label>
-                                <input type="text" className="form-control" name=" Apellido_Paterno " placeholder="Introduzca su Apellido Paterno" />
+                                <input type="text" className="form-control text-center" name=" Apellido_Paterno " placeholder="Introduzca su Apellido Paterno" />
                             </div>
                             <div className="form-group">
                                 <label for="exampleInputPassword1">Apellido Materno</label>
-                                <input type="text" className="form-control" name=" Apellido_Materno " placeholder="Introduzca su Apellido Materno" />
+                                <input type="text" className="form-control text-center" name=" Apellido_Materno " placeholder="Introduzca su Apellido Materno" />
                             </div>
                             <label for="inputPassword5" class="form-label">Contraseña</label>
-                            <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock" placeholder="Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji." />
-                            
-                            <button type="submit" className="btn btn-primary mt-2">Enviar</button>
-                            <Link className="btn btn-primary mt-2" to="/home" role="button">Regresar</Link>
+                            <input type="password" id="inputPassword5" class="form-control text-center" aria-describedby="passwordHelpBlock" placeholder="Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji." />
+
+                            <button type="submit" className="btn btn-warning mx-2 mt-2">Enviar</button>
+                            <Link className="btn btn-warning mt-2" to="/home" role="button">Regresar</Link>
                         </form>
                     </div>
                 </div>
--- src/pages/Conocenos.js
+++ src/pages/Conocenos.js
@@ -1,20 +1,12 @@
 
 import React, { Component } from 'react';
 //import { Link } from 'react-router-dom';
-//import './Estilos/conoce.scss';
+
 class Conocenos extends Component {
     render() {
         return (
             <div>
-                <div class="card text-center">
-                    <div class="card-header">
-                        Featured
-                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                </div>
+                
             </div>
         )
     }
