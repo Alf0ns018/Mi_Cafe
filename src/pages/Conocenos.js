a0a9c23abbeda473b966c0805fa192baf13d0593
Alfonso <edgaralfonsoeacg@gmail.com>
2021-05-07 16:03:58 -0500

    Agrege una caja de dialogo a conocenos como prueba

--- src/pages/Conocenos.js
+++ src/pages/Conocenos.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+//import './Estilos/conoce.scss';
 class Conocenos extends Component {
     render() {
         return (
             <div>
-                 <div class="example-container">
-                    <div class="row">
-
-                        <div class="col-auto mr-auto"> <h4><i> Pagina de conocenos</i>g</h4> </div>
-                        <div class="col-auto"> <div class="col-sm-1">  <Link className="btn btn-primary mt-1 " to="/home" role="button">Regresar</Link> </div>
-                        </div>
+                <div class="card text-center">
+                    <div class="card-header">
+                        Featured
+                     </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                     </div>
                 </div>
             </div>
