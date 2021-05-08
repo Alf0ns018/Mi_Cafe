39914528fca15ae54ae61f1c8f762b45d4b42b2a
José Rodríguez <61205529+josergz@users.noreply.github.com>
2021-05-08 13:08:40 -0500

    correccion de rutas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css'
-import Header from './componentes/inicio/header';
 import Headerv2 from './componentes/inicio/headerv2';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -16,18 +15,11 @@ class App extends Component {
     return (
      
       <Router>
-        <Header />
-        <Route path='/home' exact component={Home}/>
         <div className="container-fluid">
-<<<<<<< HEAD
           <Headerv2 />
           <br></br>
           <Route path='/home' exact component={Home} />
           <Route path='/menu' exact component={Menú} />
-=======
-          <Route path='/home' exact component={Home}/>
-          <Route path='/menu' exact component={Menú}/>
->>>>>>> dddc22bf19c82cb971ac4e35c153842a59618089
           <Route path='/conocenos' exact component={Conocenos} />
           <Route path= '/iniciars'exact component={InicioS}/>
           <Route path= '/regis'exact component={Registro}/>
--- src/pages/Conocenos.js
+++ src/pages/Conocenos.js
@@ -1,12 +1,12 @@
 
 import React, { Component } from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Conocenos extends Component {
     render() {
         return (
             <div>
-
+esta es la pagina conocenos
             </div>
 
         )
