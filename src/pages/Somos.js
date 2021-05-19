f5c07c3702875496194f18dd8dcb44eac684dd5c
Alfonso <edgaralfonsoeacg@gmail.com>
2021-05-19 11:44:57 -0500

    Campos nuevos para Quienes somos

--- src/pages/Somos.js
+++ src/pages/Somos.js
@@ -4,23 +4,68 @@ import { Link } from 'react-router-dom';
 class Somos extends Component {
     render() {
         return (
-            <div class="container">
-                <div class="card">
-                    <div class="card-body">
-                        <h1 class="card-title"><strong> ¿Quienes somos? </strong></h1>
+            <div>
 
-                        <nav class="navbar navbar-light bg-light">
-                            <a class="navbar-brand" href="#"> ¿Quienes Somos?</a> 
-                            <a class="navbar-brand" href="#"> Misión </a> 
-                            <a class="navbar-brand" href="#"> Vision </a> 
-                            <a class="navbar-brand" href="#"> Metas </a> 
-                        </nav>
+                <div class="accordion text-center" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    ¿Quienes Somos?
+                                </button>
+                            </h2>
+                        </div>
 
-
-
-                        <Link className="btn btn-warning mt-2" to="home" role="button">Regresar</Link>
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                Mi Casa Café nace de las necesidades
+                             </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingTwo">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    Metas
+                                  </button>
+                            </h2>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                            <div class="card-body">
+                                La principal meta de Mi Casa Café es hacer de los clientes
+                          </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingThree">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    Misión
+                                  </button>
+                            </h2>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                Ser portadores
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingFour">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    Visión
+                                  </button>
+                            </h2>
+                        </div>
+                        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+                            <div class="card-body">
+                                Cumplir con las necesidades
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <Link className="btn btn-warning mt-2" to="home" role="button">Regresar</Link>
             </div>
         )
     }
